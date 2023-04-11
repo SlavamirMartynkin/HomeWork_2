e0e107e944adfbb13f3609b8e4c334ca766382a8
Martynkin Slavamir <sss_mmm86@mail.ru>
2023-04-12 01:57:37 +0300

    add Task_10.1

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,6 +1,7 @@
-﻿int x = new Random().Next(100, 1000);
-System.Console.WriteLine(x);
+﻿System.Console.WriteLine("Введите число");
+string x = Console.ReadLine();
+int x1 = int.Parse(x);
 
-int second = x / 10 % 10;
+int second = x1 / 10 % 10;
 
 System.Console.WriteLine($"Вторая цифра числа {x} - это {second}.");
