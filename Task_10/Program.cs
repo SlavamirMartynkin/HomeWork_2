20d95a8d64bc2cb2aaeea841ad174c09d476217b
Martynkin Slavamir <sss_mmm86@mail.ru>
2023-04-12 01:37:41 +0300

    add Task_10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = new Random().Next(100, 1000);
+System.Console.WriteLine(x);
+
+int second = x / 10 % 10;
+
+System.Console.WriteLine($"Вторая цифра числа {x} - это {second}.");
