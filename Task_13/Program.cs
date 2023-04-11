863f8b5516f619d8c630d4a92d10ee9dba2b5c37
Martynkin Slavamir <sss_mmm86@mail.ru>
2023-04-12 01:30:59 +0300

    add Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,24 @@
+﻿System.Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int number1 = int.Parse(number);
+
+int length = 1;
+int count = 10;
+
+while (number1 / count > 0)
+{
+    length++;
+    count = count * 10;
+}
+System.Console.WriteLine($"Длина числа {length} символов.");
+
+if (length < 3)
+{
+    System.Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    count = count / 1000;
+    System.Console.WriteLine(count);
+    System.Console.WriteLine($"Третья цифра числа {number1} - это {number1 / count % 10}");
+}
