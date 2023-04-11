8752fcc24e2eedab376b03de6d83e164ba01eeb9
Martynkin Slavamir <sss_mmm86@mail.ru>
2023-04-12 01:51:55 +0300

    add Task_10

---
+++ Task_15/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите номер дня недели");
+string number = Console.ReadLine();
+int weekNumber = int.Parse(number);
+
+if (weekNumber == 6 || weekNumber == 7)
+{
+    System.Console.WriteLine($"День недели № {weekNumber} - выходной");
+}
+else if (weekNumber < 1 || weekNumber > 7)
+{
+    System.Console.WriteLine("Странный день недели");
+}
+else
+{
+    System.Console.WriteLine($"День недели № {weekNumber} - будний день");
+}
